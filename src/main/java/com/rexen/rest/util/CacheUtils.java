c689814b1b344d7c8d2a5988f6d22114aa9b4f8d
GavinHacker <chengguowei@rexen.com.cn>
2018-05-28 11:34:44 +0800

    增加工具类

--- src/main/java/com/rexen/rest/util/CacheUtils.java
+++ src/main/java/com/rexen/rest/util/CacheUtils.java
@@ -15,15 +15,44 @@ public class CacheUtils {
 
     public static final String special_suffix = "__]";
 
-    private volatile static Cache<String, String> cache = null;
+    private volatile static Cache<String, Object> cache = null;
 
-    public static Cache<String, String> instance(){
+    public static Cache<String, Object> instance(int initialCapacity, int concurrencyLevel, int expireDuration){
+        if(cache == null) {
+            cache = CacheBuilder.newBuilder()
+                    .initialCapacity(initialCapacity)
+                    .concurrencyLevel(concurrencyLevel)
+                    .expireAfterWrite(expireDuration, TimeUnit.SECONDS)
+                    .build();
+        }
+        return cache;
+    }
+
+    public static Cache<String, Object> instance(int expireDuration){
+        if(cache == null) {
+            cache = CacheBuilder.newBuilder()
+                    .initialCapacity(20)
+                    .concurrencyLevel(10)
+                    .expireAfterWrite(expireDuration, TimeUnit.SECONDS)
+                    .build();
+        }
+        return cache;
+    }
+
+    public static Cache<String, Object> instance(){
         if(cache == null) {
             cache = CacheBuilder.newBuilder()
                     .initialCapacity(20)
                     .concurrencyLevel(10)
                     .expireAfterWrite(20, TimeUnit.SECONDS)
                     .build();
+        }
+        return cache;
+    }
+
+    public static Cache<String, Object> instance(CacheBuilder<String, Object> cacheBuilder){
+        if(cache == null){
+            cache = cacheBuilder.build();
         }
         return cache;
     }
